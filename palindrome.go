74b91a3876e91b27c1922702c0c8dc39d4779e99
initirta <bachrietirta@gmail.com>
2025-10-17 18:52:32 +0700

    asyif gokil

---
+++ palindrome.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		a, c int
+		cond bool
+	)
+
+	fmt.Scan(&a)
+
+
+    reversed := 0
+    c = a
+    for a != 0 {
+        digit := a % 10         
+        reversed = reversed*10 + digit
+        a = a / 10            
+    }
+
+	cond = c == reversed
+
+	fmt.Println(cond)
+}
